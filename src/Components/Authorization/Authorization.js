019a08685e838b7a6154822fc5e2a2024e79818a
Big ML <m@y.ru>
2021-08-29 12:01:08 +0300

    add authorization cpmponent

---
+++ src/Components/Authorization/Authorization.js
@@ -0,0 +1,88 @@
+import React from 'react';
+//import { closeActivationForm } from '../authSlice';
+import './Authorization.css';
+import Registration from "../Authorization/Registration/Registration";
+import Login from "../Authorization/Login/Login";
+
+import Activation from "../Authorization/Activation/Activation";
+import {openRegistrationForm, openLoginForm} from '../Authorization/authSlice';
+
+const Authorization= (props) => {
+    const {dispatch} = props;
+
+    //RENDER COMPONENT
+  const renderRegistration = () => {
+    return (
+    <Registration 
+      dispatch={dispatch}
+      userData={props.userData}
+      />
+    );
+  };
+
+//LOGIN COMPONENT
+const renderLogin = () => {
+  return (
+  <Login 
+    dispatch={dispatch}
+    userData={props.userData}
+    />
+  );
+};
+//ACTIVATION COMPONENT
+const renderActivation = () => {
+  return (
+  <Activation 
+    dispatch={dispatch}
+    userData={props.userData}
+    />
+  );
+};
+// RENDER BUTTONS
+const buttonsForm = () => {
+    return (
+        <div className="Authorization">
+          <button onClick = {() => dispatch(openLoginForm())}>Логин</button>
+          <button onClick = {() => dispatch(openRegistrationForm())}>Регистрация</button>
+        </div>
+      );
+}
+
+const render = () =>{
+    if (props.userData.email) {
+        return (
+            <div className="Authorization">
+            <h3>Пользователь {props.userData.email}</h3>
+            <button>Выход</button>
+            
+            </div>
+          );
+         
+    } else {
+        return (
+            <div className="Authorization">
+            <button onClick = {() => dispatch(openLoginForm())}>Логин</button>
+            <button onClick = {() => dispatch(openRegistrationForm())}>Регистрация</button>
+            <h3>Пользователь {props.userData.email}</h3>
+            <button>Выход</button>
+         
+            </div>
+          );
+    }
+   
+}
+
+
+    if (props.userData.openRegistrationForm === true) {
+        return renderRegistration();
+    }
+    if (props.userData.openLoginForm === true) {
+      return renderLogin();
+  }
+    if (props.userData.openActivationForm === true) {
+      return renderActivation();
+    } 
+    return render(); 
+  }
+  
+  export default Authorization;
--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -1,15 +1,16 @@
 import React from "react";
 import './Main.css';
-import Registration from "../Authorization/Registration/Registration";
-import Login from "../Authorization/Login/Login";
+//import Registration from "../Authorization/Registration/Registration";
+//import Login from "../Authorization/Login/Login";
 import MainData from "./MainData/MainData";
-import Activation from "../Authorization/Activation/Activation";
+//import Activation from "../Authorization/Activation/Activation";
+import Authorization from "../Authorization/Authorization";
 import {openRegistrationForm, openLoginForm} from '../Authorization/authSlice';
 
 const Main = (props) => {
   const {dispatch} = props;
  // const [showRegistration, setShowRegistration] = useState(false);
-
+/*
 //RENDER COMPONENT
   const renderRegistration = () => {
     return (
@@ -38,14 +39,37 @@ const renderActivation = () => {
     />
   );
 };
+*/
+/*
 // MAIN VIEW RENDER
 const renderMainView = () => {
   return (
   <div className="Main">
     <button onClick = {() => dispatch(openLoginForm())}>Логин</button>
     <button onClick = {() => dispatch(openRegistrationForm())}>Регистрация</button>
+    <h3>Пользователь {props.userData.email}</h3>
+    <button>Выход</button>
     <p> HELLO </p>
-   
+    <Authorization
+    dispatch={dispatch}
+    userData={props.userData}
+    />
+    <MainData 
+    dispatch={dispatch}
+    userData={props.userData}
+    />
+ </div>
+
+  );
+};
+*/
+const renderMainView = () => {
+  return (
+  <div className="Main">
+    <Authorization
+    dispatch={dispatch}
+    userData={props.userData}
+    />
     <MainData 
     dispatch={dispatch}
     userData={props.userData}
@@ -57,7 +81,7 @@ const renderMainView = () => {
 
 //RENDER
 
-
+/*
   if (props.userData.openRegistrationForm === true) {
       return renderRegistration();
   }
@@ -67,7 +91,7 @@ const renderMainView = () => {
   if (props.userData.openActivationForm === true) {
     return renderActivation();
   }
-  
+ */ 
   
       return renderMainView();
 
